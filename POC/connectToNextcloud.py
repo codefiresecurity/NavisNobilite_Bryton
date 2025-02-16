2ebf0e65b196cfba2fcce6c6f3fa96dcc335b0f5
Ryan Wessels <ryan.wessels@gmail.com>
2025-02-16 20:31:30 +0000

    More POC work

--- POC/connectToNextcloud.py
+++ POC/connectToNextcloud.py
@@ -15,9 +15,10 @@ def downloadFiles(nextcloudUrl, username, password, remoteFolder, localFolder):
         if not remoteFolder.endswith('/'):
             remoteFolder += '/'
 
-        os.makedir(localFolder, exists_ok=True)
-        files = nc.list_folders(remoteFolder)
-        zipFiles = [f for f in files if f['path'].lower().endswith('.zip')]
+        os.makedirs(localFolder, exist_ok="True")
+        response = nc.list_folders(remoteFolder)
+        files = response.data
+        zipFiles = [f for f in files if f['href'].lower().endswith('.zip')]
 
         if not zipFiles:
             print(f"Found no zip files found in {remoteFolder}")
@@ -25,5 +26,20 @@ def downloadFiles(nextcloudUrl, username, password, remoteFolder, localFolder):
         
         print(f"Found {len(zipFiles)} ZIP file(s)")
 
+        for file in zipFiles:
+            filePath = file['href']
+            fileName = os.path.basename(filePath)
+            localPath = os.path.join(localFolder, fileName)
+            print(f"Downloading: {fileName}")
+            nc.download_file(remoteFolder + '/' + fileName, localPath)
+            print(f"Successfully downloaded to: {localPath}")
+
     except Exception as e:
         print(f"An error occured: {str(e)}")
+
+if __name__ == "__main__":
+
+    REMOTE_FOLDER = "Fitness"
+    LOCAL_FOLDER = "./toProcess"
+
+    downloadFiles(NEXTCLOUD_URL, USERNAME, PASSWORD, REMOTE_FOLDER, LOCAL_FOLDER)
